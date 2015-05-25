7d75561043ada41cf114a537c48815e78a9128d2
Mateusz Palyz <m.palyz@pilot.co>
2015-05-25 21:24:55 +0200

    store messages in template helper

---
+++ client/app.js
@@ -0,0 +1,8 @@
+Template.messages.helpers({
+  messages: [
+    { text: "what a cool new message" },
+    { text: "this one too" },
+    { text: "this one is even better" },
+    { text: "that is great" }
+  ]
+});
