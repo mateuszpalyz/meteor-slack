910d81e04ff3fb15a89746b175d5090fede3d767
Mateusz Palyz <m.palyz@pilot.co>
2015-05-26 10:50:32 +0200

    add messages to messages collection

--- client/app.js
+++ client/app.js
@@ -1,19 +1,18 @@
 Template.messages.helpers({
-  messages: [
-    { text: "what a cool new message" },
-    { text: "this one too" },
-    { text: "this one is even better" },
-    { text: "that is great" }
-  ]
+  messages: Messages.find({})
 });
 
 Template.footer.events({
-  "keypress input": function(e) {
-    if (e.charCode == 13) {
-      e.stopPropagation();
-      $(".message-history").append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
-      $(".input-box_text").val("");
-      return false;
+  'keypress input': function(e) {
+    var inputVal = $('.input-box_text').val();
+    if(!!inputVal) {
+      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+      if (charCode == 13) {
+        e.stopPropagation();
+        Messages.insert({text: $('.input-box_text').val()});
+        $('.input-box_text').val("");
+        return false;
+      }
     }
   }
 });
