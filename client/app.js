210543c1921d39f76dffc640f0c4fbd93d6950d1
Mateusz Palyz <m.palyz@pilot.co>
2015-05-31 19:03:30 +0200

    add timestamp and user helpers

--- client/app.js
+++ client/app.js
@@ -20,6 +20,25 @@ Template.footer.events({
   }
 });
 
+Template.registerHelper("usernameFromId", function(userId) {
+  var user = Meteor.users.findOne({_id: userId});
+  if (typeof user === "undefined") {
+    return "Anonymous";
+  }
+  if (typeof user.services.github !== "undefined") {
+    return user.services.github.username;
+  }
+  return user.username;
+});
+
+Template.registerHelper("timestampToTime", function (timestamp) {
+  var date = new Date(timestamp);
+  var hours = date.getHours();
+  var minutes = "0" + date.getMinutes();
+  var seconds = "0" + date.getSeconds();
+  return hours + ":" + minutes.substr(minutes.length-2) + ":" + seconds.substr(seconds.length-2);
+});
+
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
