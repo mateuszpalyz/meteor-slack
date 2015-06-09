5590867b78ff9ef22faa21a87150a1f4dba722e8
Mateusz Palyz <mateuszpalyz@gmail.com>
2015-06-09 11:43:50 +0200

    seed channels

--- server/seeder.js
+++ server/seeder.js
@@ -1,8 +1,19 @@
 Meteor.startup(function() {
+  Channels.remove({});
+  Channels.insert({
+    name: 'general'
+  });
+  Channels.insert({
+    name: 'random'
+  });
+
   Factory.define('message', Messages, {
     text: function() {
       return Fake.sentence();
-    }
+    },
+    user: Meteor.users.findOne()._id,
+    timestamp: Date.now(),
+    channel: 'general'
   });
 
   Messages.remove({});
