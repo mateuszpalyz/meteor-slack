8017ae4175b46f2af1f2ef21fe9afc5124d7ddee
Mateusz Palyz <mateuszpalyz@gmail.com>
2015-06-09 15:16:29 +0200

    remove setting channel in session outside of router

--- client/app.js
+++ client/app.js
@@ -36,12 +36,6 @@ Template.footer.events({
   }
 });
 
-Template.channel.events({
-  'click .channel': function (e) {
-    Session.set('channel', this.name);
-  }
-});
-
 Template.channel.helpers({
   active: function () {
     if (Session.get('channel') === this.name) {
